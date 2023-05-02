c2421682184694fe6f1a24996f6cdecd3fc736f0
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-02 09:42:29 +0700

    rename main application

--- src/main/java/com/javapersistenceapi/JavaPersistenceApiApplication.java
+++ src/main/java/com/javapersistenceapi/JavaPersistenceApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.javapersistenceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JavaPersistenceApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(JavaPersistenceApiApplication.class, args);
 	}
 
 }
